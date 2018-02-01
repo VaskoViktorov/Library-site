fdf5f8a6dbb6556e851c99b288177c671f52b135
VaskoViktorov <vaskoviktorov@gmail.com>
2018-02-01 14:09:35 +0200

    More work on the site layout

--- Library.Services/ServicesConstants.cs
+++ Library.Services/ServicesConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int ArticlesPageSize = 5;
         public const int BooksPageSize = 12;
-        public const int GalleriesPageSize = 9;
+        public const int GalleriesPageSize = 6;
 
         public const int LatestBooksAmount = 6;
         public const int LatestGalleriesAmount = 2;
